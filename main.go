feda39b6cdeab8b6701a4b8556c3f219a2ab1db1
z01L <55188591+z01L@users.noreply.github.com>
2020-02-04 12:14:14 +1300

    Update main.go
    No hello

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Goodbye Cruel World!!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
